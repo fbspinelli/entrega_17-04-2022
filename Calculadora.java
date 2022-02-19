f2fb7f0bc50991359d4bafefadd9e8af1f1baad2
Fernando <fernando.b.spinelli@gmail.com>
2022-02-18 22:00:24 -0300

    Subtração concluida

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,9 @@
 public class Calculadora{
-    
+
     public double soma (double num1, double num2){
         return num1 + num2;
+    }
+    public double subtracao (double num1, double num2){
+        return num1 - num2;
     }
 }
